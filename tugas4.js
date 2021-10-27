db2b491fe0b4120c75d41ec63539666fe7beb4d5
ihlas_sul <ihlassmada@gmail.com>
2021-10-27 09:29:44 +0800

    tugas 4 javascript
    done

---
+++ tugas4.js
@@ -0,0 +1,14 @@
+let ihlas = 170
+let sul = 160
+let akbar = 150
+if(ihlas>sul){
+    console.log('ihlas yg tertinggi')
+}else if(sul>akbar){
+    if(sul>ihlas){
+        console.log('sul yg tertinggi')
+    }else{
+        console.log('sul tertinggi kedua')
+    }
+}else{
+    console.log('akbar yg tertinggi')
+}
